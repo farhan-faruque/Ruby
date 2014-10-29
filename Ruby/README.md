6535bbc9a8345d9a721cfdabcdf1115a5234e948
Farhan Faruque <farhan@farhan-cefalo.(none)>
2014-10-29 15:15:34 +0600

    add rails

---
+++ Ruby/README.md
@@ -0,0 +1,8 @@
+Ruby
+=====
+
+ * [Understanding Ruby Blocks, Procs and Lambdas](http://www.reactive.io/tips/2008/12/21/understanding-ruby-blocks-procs-and-lambdas/)
+ * [Ruby Monk](https://rubymonk.com/)
+
+Rails
+=====
---
+++ rubyonrails/rubyonrails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyonrails::Application.config.secret_token = 'f06730421f69a70e045baade4520d756d5f24b6cdb19f33eadb46777313de4fba462420e66c114d200f275111897023fcacbd1e28142c42dfcd3dae306872836'
